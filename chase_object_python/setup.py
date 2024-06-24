576d8e7b0f60fa19ed3f8b8bc163ca4b5a0fab25
Tofunmi Sodimu <35805326+TofunmiSodimu@users.noreply.github.com>
2024-06-24 12:41:28 -0400

    Add files via upload

---
+++ chase_object_python/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+package_name = 'chase_object_python'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Tofunmi Sodimu',
+    maintainer_email='tofsodimu@gmail.com',
+    description='chase an object',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = chase_object_python.detect_object:main',
+            'listener_1 = chase_object_python.get_object_range:main',
+            'listener_2 = chase_object_python.chase_object:main',
+        ],
+    },
+)
